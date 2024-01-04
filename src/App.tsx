63bd8fa93e182e15ec4609af95ae4a1c4ef00996
Arturo-MT <ingatorrez@gmail.com>
2024-01-04 16:06:21 -0600

    Refactor App component and add react-query and react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,33 @@
-import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import { FetchProvider } from './context/fetch'
+import Home from 'pages/home'
+import MainLayout from './containers/main-layout'
 
-export function CardWithForm() {
-  return (
-    <Card className="w-[350px]">
-      <CardHeader>
-        <CardTitle>Create project</CardTitle>
-        <CardDescription>Deploy your new project in one-click.</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form>
-          <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" placeholder="Name of your project" />
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="framework">Framework</Label>
-              <Select>
-                <SelectTrigger id="framework">
-                  <SelectValue placeholder="Select" />
-                </SelectTrigger>
-                <SelectContent position="popper">
-                  <SelectItem value="next">Next.js</SelectItem>
-                  <SelectItem value="sveltekit">SvelteKit</SelectItem>
-                  <SelectItem value="astro">Astro</SelectItem>
-                  <SelectItem value="nuxt">Nuxt.js</SelectItem>
-                </SelectContent>
-              </Select>
-            </div>
-          </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex justify-between">
-        <Button variant="outline">Cancel</Button>
-        <Button>Deploy</Button>
-      </CardFooter>
-    </Card>
-  )
-}
+const { VITE_REACT_APP_API_URL } = import.meta.env
+
+console.info(VITE_REACT_APP_API_URL)
 
 export default function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false
+      }
+    }
+  })
+
   return (
-    <div className="flex items-center justify-center h-screen bg-black dark">
-      <CardWithForm />
-    </div>
+    <Router>
+      <FetchProvider>
+        <QueryClientProvider client={queryClient}>
+          <MainLayout>
+            <Routes>
+              <Route path='/' element={<Home />} />
+            </Routes>
+          </MainLayout>
+        </QueryClientProvider>
+      </FetchProvider>
+    </Router>
   )
 }
