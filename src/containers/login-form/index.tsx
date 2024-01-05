e79ed5dba05d769348c6ff9b10c1c17456bade42
Arturo-MT <ingatorrez@gmail.com>
2024-01-04 19:03:05 -0600

    Add login form and login page

---
+++ src/containers/login-form/index.tsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
+import { LoginFormValues } from '@/src/types'
+import { Button } from '@/components/ui/button'
+import { Card } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { useAuth } from '@/src/context/auth'
+
+const LoginSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(2, 'El correo electrónico es muy corto')
+    .max(50, 'El correo electrónico es muy largo')
+    .required('El correo electrónico es obligatorio'),
+  password: Yup.string()
+    .min(2, 'La contraseña es muy corta')
+    .max(50, 'La contraseña es muy larga')
+    .required('La contraseña es obligatoria')
+})
+
+const LoginForm: React.FC = () => {
+  const initialValues: LoginFormValues = { username: '', password: '' }
+  const { loginWithPassword } = useAuth()
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={LoginSchema}
+      onSubmit={(values, actions) => {
+        loginWithPassword(values)
+        actions.setSubmitting(false)
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <Card className='flex flex-col gap-4 p-8 w-max bg-slate-50'>
+            <Field
+              type='text'
+              name='username'
+              placeholder='Nombre de usuario'
+              as={Input}
+            />
+            <ErrorMessage
+              name='username'
+              component={Label}
+              className='text-red-400'
+            />
+
+            <Field
+              type='password'
+              name='password'
+              placeholder='Contraseña'
+              as={Input}
+            />
+            <ErrorMessage
+              name='password'
+              component={Label}
+              className='text-red-400'
+            />
+
+            <Button type='submit' disabled={isSubmitting}>
+              Iniciar sesión
+            </Button>
+          </Card>
+        </Form>
+      )}
+    </Formik>
+  )
+}
+
+export default LoginForm
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,9 @@
+import LoginForm from '@/src/containers/login-form'
+
+export default function Login() {
+  return (
+    <div className='flex flex-col items-center'>
+      <LoginForm />
+    </div>
+  )
+}
