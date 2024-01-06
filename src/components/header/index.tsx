6a2137a72e5e4eac47d708fea1d85c6ad4425262
Arturo-MT <ingatorrez@gmail.com>
2024-01-05 18:20:23 -0600

    Add logout functionality to header component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,10 @@
 import { Link } from 'react-router-dom'
+import { Button } from '@/components/ui/button'
+import { useAuth } from '@/src/context/auth'
 
 export default function Header() {
+  const isUserLogged = localStorage.getItem('TL_USER') !== null
+  const { logout } = useAuth()
   return (
     <header className='flex items-center justify-between w-full h-20 px-6 bg-white dark:bg-black'>
       <div className='flex items-center h-full'>
@@ -14,13 +18,17 @@ export default function Header() {
         </Link>
       </div>
       <div className='flex items-center h-full'>
-        <Link to='/login'>
-          <span className='flex items-center justify-between h-full'>
-            <span className='ml-2 text-xl font-bold text-black dark:text-white'>
-              Login
+        {isUserLogged ? (
+          <Button onClick={logout}>Logout</Button>
+        ) : (
+          <Link to='/login'>
+            <span className='flex items-center justify-between h-full'>
+              <span className='text-xl font-bold text-black dark:text-white'>
+                Iniciar sesión
+              </span>
             </span>
-          </span>
-        </Link>
+          </Link>
+        )}
       </div>
     </header>
   )
