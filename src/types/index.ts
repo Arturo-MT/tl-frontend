07d5521a76df23d433efc51e942bb0cd296ce8da
Arturo-MT <ingatorrez@gmail.com>
2024-01-05 18:19:30 -0600

    Add email field to LoginFormValues and loginError field to StateType

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,11 +7,13 @@ export interface StoreInterface {
 export interface LoginFormValues {
   username: string
   password: string
+  email?: string
 }
 
 export interface StateType {
   user: UserType | null
   loading: boolean
+  loginError: string | null
 }
 
 export interface UserType {
@@ -24,6 +26,10 @@ export type ActionType =
   | { type: 'FETCH_USER' }
   | { type: 'SET_USER'; payload: { user: UserType } }
   | { type: 'REMOVE_USER' }
+  | {
+      type: 'SET_LOGIN_ERROR'
+      payload: { loginError: string | null }
+    }
 
 export interface AuthContextType {
   loginWithPassword: (values: LoginFormValues) => Promise<void>
@@ -31,4 +37,6 @@ export interface AuthContextType {
   user: UserType | null
   loading: boolean
   renewToken: () => Promise<void>
+  loginError: string | null
+  registerUser: (values: LoginFormValues) => Promise<void>
 }
