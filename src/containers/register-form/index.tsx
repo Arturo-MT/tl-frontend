5cc2219ef470e8c28e4b0932ba2f8e4088834e67
Arturo-MT <ingatorrez@gmail.com>
2024-02-13 19:38:31 -0600

    Añadir campo "is_seller" al formulario de registro

--- src/containers/register-form/index.tsx
+++ src/containers/register-form/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Formik, Form, Field, ErrorMessage } from 'formik'
 import * as Yup from 'yup'
+
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
@@ -11,6 +12,7 @@ interface UserFormValues {
   email: string
   password: string
   confirmPassword?: string
+  is_seller: boolean
 }
 
 const UserSchema = Yup.object().shape({
@@ -26,14 +28,16 @@ const UserSchema = Yup.object().shape({
     .required('Requerido'),
   confirmPassword: Yup.string()
     .oneOf([Yup.ref('password'), ''], 'Las contraseñas deben coincidir')
-    .required('Confirmación de contraseña requerida')
+    .required('Confirmación de contraseña requerida'),
+  isSeller: Yup.boolean()
 })
 
 const RegisterForm: React.FC = () => {
   const initialValues: UserFormValues = {
     username: '',
     email: '',
-    password: ''
+    password: '',
+    is_seller: false
   }
 
   const { registerUser } = useAuth()
@@ -91,6 +95,13 @@ const RegisterForm: React.FC = () => {
             component={Label}
             className='text-red-400'
           />
+
+          <div className='flex space-x-3'>
+            <Field type='checkbox' name='is_seller' />
+            <Label htmlFor='is_seller' className='text-sm'>
+              ¿Crear cuenta de vendedor?
+            </Label>
+          </div>
 
           <Button type='submit' disabled={isSubmitting}>
             Registrarse
