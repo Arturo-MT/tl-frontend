85f57a94b307f3594f0ee56413eb01532106bbaf
Arturo-MT <ingatorrez@gmail.com>
2024-02-13 18:52:59 -0600

    Eliminar página de inicio de sesión

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { AuthProvider } from './context/auth'
 
 import MainLayout from 'containers/main-layout'
 import Home from 'pages/home'
-import Login from 'pages/login'
 import StoreDetails from 'pages/store-details'
 
 const { VITE_REACT_APP_API_URL } = import.meta.env
@@ -30,7 +29,6 @@ export default function App() {
               <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='*' element={<div>404</div>} />
-                <Route path='/login' element={<Login />} />
                 <Route path='/:store' element={<StoreDetails />} />
               </Routes>
             </MainLayout>
