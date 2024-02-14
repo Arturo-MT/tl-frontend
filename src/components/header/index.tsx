e7b5473ced3e8acf70e9d90c09f4483123685aa7
Arturo-MT <ingatorrez@gmail.com>
2024-02-13 18:52:03 -0600

    Añadir menú desplegable y hoja de inicio de sesión en el encabezado

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,28 @@
 import { Link } from 'react-router-dom'
+
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger
+} from '@/components/ui/sheet'
+import { Tabs, TabsList, TabsTrigger, TabsContent } from '@/components/ui/tabs'
+
+import LoginForm from '@/src/containers/login-form'
+import RegisterForm from '@/src/containers/register-form'
+
 import { useAuth } from '@/src/context/auth'
 
 export default function Header() {
@@ -7,7 +30,7 @@ export default function Header() {
   const user = JSON.parse(localStorage.getItem('TL_USER') || '{}')
   const { logout } = useAuth()
   return (
-    <header className='flex items-center justify-between w-full h-20 px-6 bg-white'>
+    <header className='flex items-center justify-between w-full h-20 px-6'>
       <div className='flex items-center h-full'>
         <Link to='/'>
           <span className='flex items-center justify-between h-full'>
@@ -18,22 +41,72 @@ export default function Header() {
           </span>
         </Link>
       </div>
-      <div className='flex items-center h-full'>
+      <div className='px-6'>
         {isUserLogged ? (
-          <>
-            <p>
-              Hola, <strong>{user.username || 'A'}</strong>
-            </p>
-            <Button onClick={logout}>Logout</Button>
-          </>
+          <div className=''>
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Avatar>
+                  {user.avatar ? (
+                    <AvatarImage src={user.avatar} alt={user.name} />
+                  ) : (
+                    <AvatarFallback>
+                      {user.username.charAt(0).toUpperCase()}
+                    </AvatarFallback>
+                  )}
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>{user.username}</DropdownMenuLabel>
+                <Link to='/profile'>
+                  <DropdownMenuItem>Perfil</DropdownMenuItem>
+                </Link>
+                {user.is_seller ? (
+                  <Link to='/admin-stores'>
+                    <DropdownMenuItem>Administrar Tiendas</DropdownMenuItem>
+                  </Link>
+                ) : (
+                  <Link to='/orders'>
+                    <DropdownMenuItem>Ver órdenes</DropdownMenuItem>
+                  </Link>
+                )}
+                <Link to='/settings'>
+                  <DropdownMenuItem>Configuración</DropdownMenuItem>
+                </Link>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem onClick={logout}>
+                  Cerrar sesión
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
         ) : (
-          <Link to='/login'>
-            <span className='flex items-center justify-between h-full'>
-              <span className='text-xl font-bold text-black dark:text-white'>
-                Iniciar sesión
-              </span>
-            </span>
-          </Link>
+          <Sheet>
+            <SheetTrigger>
+              <Button>Ingresar</Button>
+            </SheetTrigger>
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle>Ingresar</SheetTitle>
+                <SheetDescription>
+                  Accede a tu cuenta para ver tus pedidos o administrar tus
+                  tiendas.
+                </SheetDescription>
+              </SheetHeader>
+              <Tabs defaultValue='login' className='my-4'>
+                <TabsList>
+                  <TabsTrigger value='login'>Ingresar</TabsTrigger>
+                  <TabsTrigger value='register'>Registrarse</TabsTrigger>
+                </TabsList>
+                <TabsContent value='login'>
+                  <LoginForm />
+                </TabsContent>
+                <TabsContent value='register'>
+                  <RegisterForm />
+                </TabsContent>
+              </Tabs>
+            </SheetContent>
+          </Sheet>
         )}
       </div>
     </header>
